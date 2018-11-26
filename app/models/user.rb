3696c572d9e8aec0f49d1cf3c509713a76dd4169
Jeremy Jouffroy <jeremy.jouffroy@gmail.com>
2018-11-26 16:01:11 +0100

    ajout validations dans les modeles

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :bookings
+  has_many :mobiles
+  has_many :reviews, through: :bookings
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :phone_number, presence: true
+  validates :address, presence: true
+  validates :postcode, presence: true
+  validates :city, presence: true
+  validates :country, presence: true
+
 end
