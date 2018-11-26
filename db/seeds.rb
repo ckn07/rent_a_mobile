575223426c9a103e9fe7d35793d2e73ab81e16cb
Jeremy Jouffroy <jeremy.jouffroy@gmail.com>
2018-11-26 17:47:55 +0100

    ajout des users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts 'Cleaning database of users...'
+User.destroy_all
+
+puts 'generating the best users with password 123456 '
+jeremy = User.create!(email: "jeremy.jouffroy@gmail.com",
+ password: "123456",
+ first_name: "Jeremy",
+ last_name: "Jouffroy",
+ phone_number: "0683411488",
+ address: "26 rue Edouard Vaillant",
+ postcode: "93140",
+ city: "Bondy",
+ country: "France")
+benoit = User.create!(email: "benoitdelest@gmail.com",
+ password: "123456",
+ first_name: "Benoit",
+ last_name: "Delest",
+ phone_number: "0609216014",
+ address: "3 rue de la Joncquiere",
+ postcode: "75017",
+ city: "Paris",
+ country: "France")
+cecile = User.create!(email: "cecile.dezy@gmail.com",
+ password: "123456",
+ first_name: "Cécile",
+ last_name: "Dezy",
+ phone_number: "0677136554",
+ address: "6 rue Euryale Dehaynin",
+ postcode: "75019",
+ city: "Paris",
+ country: "France")
+chris = User.create!(email: "christophe@noujaim.com",
+ password: "123456",
+ first_name: "Christophe",
+ last_name: "Noujaim",
+ phone_number: "0663420512",
+ address: "Pigalle 69b",
+ postcode: "75018",
+ city: "Paris",
+ country: "France")
+
+
+
