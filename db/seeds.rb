9622dc4022eadbb1ae69b78f8ca0b92a3158dd1a
Jeremy Jouffroy <jeremy.jouffroy@gmail.com>
2018-11-26 18:56:26 +0100

    Ajout seeds sur booking et review

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
 puts 'Cleaning database of users...'
 User.destroy_all
+puts 'Cleaning database of Mobile...'
+Mobile.destroy_all
+puts 'Cleaning database of bookings...'
+Booking.destroy_all
+puts 'Cleaning database of Reviews...'
+Review.destroy_all
 
 puts 'generating the best users with password 123456 '
 jeremy = User.create!(email: "jeremy.jouffroy@gmail.com",
@@ -50,61 +55,109 @@ chris = User.create!(email: "christophe@noujaim.com",
 
 puts "4 users created"
 
-puts 'Cleaning database of Mobile...'
-Mobile.destroy_all
 
 puts "creating 1 mobile for each user"
-mobone = Mobile.new(brand: "Samsung",
+mobOne = Mobile.new(brand: "Samsung",
   model: "Note 8",
   daily_price: "5",
-  photo: "myr1ofzlfvgayoy7bmey"
+  photo: "myr1ofzlfvgayoy7bmey",
   address: "26 rue Edouard Vaillant",
   postcode: "93140",
   city: "Bondy",
   country: "France")
-mobone.user_id = jeremy.id
-mobone.save!
+mobOne.user_id = jeremy.id
+mobOne.save!
 
-mobtwo = Mobile.new(brand: "Apple",
+mobTwo = Mobile.new(brand: "Apple",
   model: "Iphone 7 +",
   daily_price: "2",
-  photo: "myr1ofzlfvgayoy7bmey"
+  photo: "myr1ofzlfvgayoy7bmey",
   address: "3 rue de la Joncquiere",
   postcode: "75017",
   city: "Paris",
   country: "France")
-mobtwo.user_id = benoit.id
-mobtwo.save!
+mobTwo.user_id = benoit.id
+mobTwo.save!
 
 
-mobthree = Mobile.new(brand: "Samsung",
+mobThree = Mobile.new(brand: "Samsung",
   model: "galaxy s7",
   daily_price: "1",
-  photo: "myr1ofzlfvgayoy7bmey"
+  photo: "myr1ofzlfvgayoy7bmey",
   address: "6 rue Euryale Dehaynin",
   postcode: "75019",
   city: "Paris",
   country: "France")
-mobthree.user_id = cecile.id
-mobthree.save!
+mobThree.user_id = cecile.id
+mobThree.save!
 
-mobfour = Mobile.new(brand: "One+",
+mobFour = Mobile.new(brand: "One+",
   model: "3T",
   daily_price: "3",
-  photo: "myr1ofzlfvgayoy7bmey"
+  photo: "myr1ofzlfvgayoy7bmey",
   address: "Pigalle 69b",
   postcode: "75018",
   city: "Paris",
   country: "France")
-mobfour.user_id = chris.id
-mobfour.save!
+mobFour.user_id = chris.id
+mobFour.save!
 
 
 puts "4 mobiles saved"
 
 
-puts 'Cleaning database of bookings...'
-Booking.destroy_all
+
+
+
+puts 'Creating a booking from Jeremy phones to Benoit.'
+
+bookingOne = Booking.new(
+minDate: Faker::Date.backward(14),
+maxDate: Date.today
+  )
+bookingOne.duration = (bookingOne.maxDate - bookingOne.minDate).to_i
+bookingOne.mobile_id = mobOne.id
+bookingOne.total_price = (mobOne.daily_price * bookingOne.duration).to_i
+bookingOne.user_id = benoit.id
+bookingOne.save!
+puts "Wow Jeremy has rented his phones to Benoit from #{bookingOne.minDate} to #{bookingOne.maxDate} for #{bookingOne.total_price}"
+
+
+
+puts 'Creating a booking from Chris phones to Cecile.'
+
+bookingTwo = Booking.new(
+minDate: Faker::Date.backward(14),
+maxDate: Date.today
+  )
+bookingTwo.duration = (bookingTwo.maxDate - bookingTwo.minDate).to_i
+bookingTwo.mobile_id = mobFour.id
+bookingTwo.total_price = (mobFour.daily_price * bookingTwo.duration).to_i
+bookingTwo.user_id = cecile.id
+bookingTwo.save!
+puts "Wow Chris has rented his phones to Cecile from #{bookingTwo.minDate} to #{bookingTwo.maxDate} for #{bookingTwo.total_price}"
+
+
+
+puts "Creating a review from Benoit about jeremy phone"
+reviewOne = Review.new(
+  content: "#{Faker::FamousLastWords.last_words*2}",
+  star: 4
+  )
+reviewOne.booking_id = bookingOne.user_id
+reviewOne.save!
+
+puts "Review created by Benoit"
+
+puts "Creating a booking from Chris phones to Cecile."
+reviewTwo = Review.new(
+  content: "#{Faker::FamousLastWords.last_words*2}",
+  star: 5
+  )
+reviewTwo.booking_id = bookingTwo.user_id
+reviewTwo.save!
+puts "Review created by Cecile "
+
 
 
 
