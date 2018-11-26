130483bf508d603e2031119bd4da3453596b57f8
Jeremy Jouffroy <jeremy.jouffroy@gmail.com>
2018-11-26 15:40:52 +0100

    ajout de 3 models booking mobile review et ajout column sur Users

---
+++ app/models/mobile.rb
@@ -0,0 +1,3 @@
+class Mobile < ApplicationRecord
+  belongs_to :user
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,47 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_26_135605) do
+ActiveRecord::Schema.define(version: 2018_11_26_143833) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "minDate"
+    t.date "maxDate"
+    t.integer "total_price"
+    t.bigint "user_id"
+    t.bigint "mobile_id"
+    t.integer "duration"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["mobile_id"], name: "index_bookings_on_mobile_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "mobiles", force: :cascade do |t|
+    t.string "brand"
+    t.string "model"
+    t.bigint "user_id"
+    t.integer "daily_price"
+    t.string "photo"
+    t.string "address"
+    t.string "postcode"
+    t.string "city"
+    t.string "country"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_mobiles_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.integer "star"
+    t.bigint "booking_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["booking_id"], name: "index_reviews_on_booking_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,8 +60,19 @@ ActiveRecord::Schema.define(version: 2018_11_26_135605) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "phone_number"
+    t.string "address"
+    t.string "postcode"
+    t.string "city"
+    t.string "country"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "mobiles"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "mobiles", "users"
+  add_foreign_key "reviews", "bookings"
 end
