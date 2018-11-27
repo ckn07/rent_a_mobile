4e82f49365ce3a47283d291513f30af32288347c
Jeremy Jouffroy <jeremy.jouffroy@gmail.com>
2018-11-27 14:33:37 +0100

    fomulaire fonctionnel mais UX horrible

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,4 +5,30 @@ class BookingsController < ApplicationController
   def show
   end
 
+  def new
+    @mobile = Mobile.find(params[:mobile_id])
+    @user = current_user
+    @booking = Booking.new
+  end
+
+  def create
+    @mobile = Mobile.find(params[:mobile_id])
+    @user = current_user
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to mobiles_path(@mobile)
+    else
+      render "new"
+    end
+  end
+
+private
+  def booking_params
+    params.require(:booking).permit(:minDate,
+    :maxDate,
+    :total_price,
+    :user_id,
+    :mobile_id,
+    :duration)
+  end
 end
