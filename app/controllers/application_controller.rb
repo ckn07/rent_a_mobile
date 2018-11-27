6bcf877b134d427506308ad0d01fdc73e86e9bae
Cecile Dezy <cecile.dezy@gmail.com>
2018-11-27 15:18:51 +0100

    change name of country to user_location on application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :address, :postcode, :city, :country, :phone_number])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :address, :postcode, :city, :user_location, :phone_number])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :address, :postcode, :city, :country, :phone_number])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :address, :postcode, :city, :user_location, :phone_number])
   end
 end
