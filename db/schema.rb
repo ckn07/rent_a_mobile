2fce67549bf5590ef10e7708dcd4d63fe962fca1
Jeremy Jouffroy <jeremy.jouffroy@gmail.com>
2018-11-28 10:30:57 +0100

    suppression des champs d'aaddresse non necessaire

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_27_141446) do
+ActiveRecord::Schema.define(version: 2018_11_28_092748) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,7 +35,6 @@ ActiveRecord::Schema.define(version: 2018_11_27_141446) do
     t.integer "daily_price"
     t.string "photo"
     t.string "address"
-    t.string "postcode"
     t.string "city"
     t.string "mobile_location"
     t.datetime "created_at", null: false
@@ -65,7 +64,6 @@ ActiveRecord::Schema.define(version: 2018_11_27_141446) do
     t.string "last_name"
     t.string "phone_number"
     t.string "address"
-    t.string "postcode"
     t.string "city"
     t.string "user_location"
     t.index ["email"], name: "index_users_on_email", unique: true
