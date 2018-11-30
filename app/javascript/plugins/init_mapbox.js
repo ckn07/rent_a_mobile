d206a2dd4359f618894d99232d6e039d7fd68eaa
Benoit Delest <benoitdelest@gmail.com>
2018-11-30 13:04:12 +0100

    zoom et marker ok sur show mobile

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -11,19 +11,34 @@ const buildMap = () => {
 };
 
 const addMarkersToMap = (map, markers) => {
-  markers.forEach((marker) => {
+  if (markers.length > 1) {
+    markers.forEach((marker) => {
+      const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
+      new mapboxgl.Marker()
+        .setLngLat([ marker.lng, marker.lat ])
+        .setPopup(popup)
+        .addTo(map);
+    });
+  } else {
+    const marker = markers
     const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
     new mapboxgl.Marker()
       .setLngLat([ marker.lng, marker.lat ])
       .setPopup(popup)
       .addTo(map);
-  });
+  }
 };
 
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
-  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
+  if (markers.length > 1) {
+    markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+    map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
+  } else {
+    const marker = markers
+    bounds.extend([ marker.lng, marker.lat ]);
+    map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
+  }
 };
 
 const initMapbox = () => {
