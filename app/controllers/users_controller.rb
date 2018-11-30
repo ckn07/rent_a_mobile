017d58e9cef88b2f91244c8d05c45220bd132ad5
Jeremy Jouffroy <jeremy.jouffroy@gmail.com>
2018-11-30 13:18:18 +0100

    controller tout puissant

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,9 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     @mobiles = Mobile.where(user_id: params[:id])
-    @booking_as_renter = Booking.where(user_id: params[:id])
-
-    @booking_as_owner = Booking.where(mobile_id: @mobiles.id)
-
-    # @user_bookings_as_lender = Booking.where()
+    @bookings_as_renter = @user.bookings
+    @bookings_as_lender = Booking.where(mobile_id: @mobiles.each { |e| e.id })
     authorize @user
   end
-
 
 end
